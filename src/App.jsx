25bfc13fa14a294c08a4a90484d27cc2f09fb207
Carlie <carliemaloneco@gmail.com>
2024-07-18 11:07:48 -0600

    setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import {puppyList} from './data.js'
-import {useState} from 'react'
+// import {puppyList} from './data/data';
+// import {useState} from 'react';
+import React from "react"
+import Home from "./pages/Home"
+import NavBar from "./pages/NavBar"
 
-function App() {
-    const [featPupId, setFeatPupId] = useState(null);
-    const [puppies, setPuppies] = useState(puppyList);
-    const featuredPup = puppies.find((pup) => pup.id === featPupId)
-    console.log(featuredPup);
-    return (
-      <div className="App">
-        {puppies.map((puppy) => {
-               return (
-               <p className='pupName' onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
-                  {puppy.name}
-                </p>
-               );
-             })
-         }
-        {featPupId && ( 
-        <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
-      </div>
-    );
-  }
+const App = () => {
+
+  return (
+    <>
+    <h1 className='text-white-p4'>Hey</h1>
+      <NavBar/>
+      <Home/>
+    </>
+  )
+}
 
 export default App
---
+++ src/pages/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => {
+
+    return (
+        <>
+            <h1>Hello NavBar</h1>
+        </>
+    )
+}
+
+export default NavBar;
