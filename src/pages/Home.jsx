88d9a19bc10ac6cf6daf71d94c32eea193682bf2
Carlie <carliemaloneco@gmail.com>
2024-07-23 16:37:39 -0600

    added Why Us Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ import Rock1 from '../assets/Ellipse 1.png'
 import Rock2 from '../assets/Ellipse 2.png'
 import Rock3 from '../assets/Ellipse 3.png'
 import Logo from '../assets/Logo Placeholder.png'
-import WhyUs from '../assets/Blur.png'
+import Photo from '../assets/Photo.png'
 
 const Home = () => {
+    // https://blennd.com/
+    // https://uiverse.io/buttons?t=tailwind&page=1
 
     return (
         <>
@@ -18,15 +20,24 @@ const Home = () => {
                         <h1 className='text-[100px] pt-[190px] text-center'>
                             Welcome to Pilates!
                         </h1>
-                            {/* <h3 className='flex'>
-                                Lets get Elevated!
-                            </h3> */}
                 </div>
             </div>
             <div className='container'>
-                <section className='grid items-end'>
-                    <img className='w-screen h-10' src={WhyUs} alt='Why Us Box'/>
-                </section>
+                <div className='homeGrid'>
+                    <div className='flex flex-col gap-32 p-8'>
+                        <div className='gap-8'>
+                            <h3 className='relative mb-4 text-5xl flex items-center flex-col text-black gap-1 m-auto'>
+                                Why Us?
+                            </h3>
+                                <p className='flex items-center justify-center'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                </p>
+                            <div className=''>
+                                <img className='w-[150px] h-[150px]' src={Photo} alt='Photo'/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
