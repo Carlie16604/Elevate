cd7f5d9dde87b5b40b317cc3c2cf798976337e47
Carlie <carliemaloneco@gmail.com>
2024-08-01 18:53:09 -0600

    reworked Footer

--- src/pages/Components/Footer.jsx
+++ src/pages/Components/Footer.jsx
@@ -7,15 +7,17 @@ import Mail from '../../assets/Reviews/Mail.png';
 const Footer = () => {
     return (
         <>
-            <div className='bg-[#949978] opacity-[75%] z-[-1] py-[3rem]'>
-                <div className='container px-8 w-screen'>
-                    <div classNAme='flex justify-between min620:flex-col min620:items-center min620:text-center min620:gap-[5rem]'>
-                        <div className='flex flex-col w-1/3 gap-8'>
+            <div className='bg-[#949978] opacity-[75%] z-[-1] py-[3rem] w-screen'>
+                <div className='container px-8'>
+                    <div className='flex justify-between min620:flex-col min620:items-center min620:text-center min620:gap-[5rem]'>
+                        <div className='flex flex-col w-[50%] gap-7'>
                             {/* Logo here */}
-                            <p className="text-[20px] color-[#000] font-medium opacity-100">
+                            <p className='text-[25px] font-bold'>Studio Policies</p>
+                            <p className='text-[25px] font-bold'>FAQs</p>
+                            <p className="text-[18px] color-[#000] font-medium opacity-100">
                             Take your health and body to the next level with our comprehensive program designed to help you reach your fitness goals.
                             </p>
-                            <div className='flex flex-wrap'>
+                            <div className='flex flex-wrap justify-between'>
                                 <div className='flex text-[20px] pt-2 w-[65px] h-[65px] justify-center  rounded-full bg-[#D4CFC9]'>
                                     <img className='w-[50px] h-[50px] justify-center align-center' src={Mail} alt='mail'/>
                                 </div>
@@ -29,6 +31,45 @@ const Footer = () => {
                                     <img className='w-[50px] h-[50px] justify-center align-center' src={FB} alt='Facebook'/>
                                 </div>
                             </div>
+                        </div>
+                        <div className='flex flex-col gap-7 relative'>
+                            <p className='text-[22px] font-bold footer-main'>
+                                Our Classes
+                            </p>
+                            <span className='top-[33px] absolute w-[60px] h-[4px] bg-[#3E5C47]'></span>
+                            <p className='text-[16px] hover:text-[#D4CFC9] cursor-pointer text-[#000] font-medium hover:font-bold'>
+                                Foundation
+                            </p>
+                            <p className='text-[16px] hover:text-[#D4CFC9] cursor-pointer text-[#000] font-medium hover:font-bold'>
+                                Water
+                            </p>
+                            <p className='text-[16px] hover:text-[#D4CFC9] cursor-pointer text-[#000] font-medium hover:font-bold'>
+                                Fire
+                            </p>
+                            <p className='text-[16px] hover:text-[#D4CFC9] cursor-pointer text-[#000] font-medium hover:font-bold'>
+                                Air
+                            </p>
+                        </div>
+                        <div className='flex flex-col gap-7 relative'>
+                        <p className='text-[22px] font-bold footer-main'>
+                                Working Hours
+                            </p>
+                            <span className='top-[33px] absolute w-[60px] h-[4px] bg-[#3E5C47]'></span>
+                            <p className='text-[16px]  text-[#000] font-bold'>
+                                Monday - Friday:
+                            </p>
+                            <p className='text-[16px] text-[#000] font-medium'>
+                                9:00am - 7:00pm
+                            </p>
+                            <p className='text-[16px]  text-[#000] font-bold'>
+                                Saturday:
+                            </p>
+                            <p className='text-[16px] text-[#000] font-medium'>
+                                9:00am - 2:00pm
+                            </p>
+                            <p className='text-[16px]  text-[#000] font-bold'>
+                               Sunday - Closed
+                            </p>
                         </div>
                     </div>
                </div>
