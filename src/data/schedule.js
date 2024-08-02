147b50bec4c8ead3add1422eda3ae8d264133306
Carlie <carliemaloneco@gmail.com>
2024-08-02 11:42:33 -0600

    updated API

--- src/data/schedule.js
+++ src/data/schedule.js
@@ -1,58 +1,56 @@
-import { classes } from './classes';
-
 export const schedule = [
-    {
-      id: 1,
-      day: "monday",
-      classes: [
-        {id: 1, typeId, time: '9:00am - 10:00am', instructor: 'Lara Croft'},
-        {id: 2, typeId, time: '1:00pm - 2:00pm', instructor: 'Jake Sully'},
-        {id: 3, typeId, time: '5:30pm - 6:30pm', instructor: 'Hannah Cage '}
-      ]
-    },
-    {
-      id: 2,
-      day: "tuesday",
-      classes: [
-        {id: 4, typeId, time: '10:30pm - 11:30pm', instructor: "Sam Flynn"},
-        {id: 5, typeId, time: "1:00pm - 2:00pm", instructor: 'Hannah Cage'},
-        {id: 6, typeId, time: '5:30pm - 6:30pm', instructor: "Kayla Price"}
-      ]
-    },
-    {
-      id: 3,
-      day: "wednesday",
-      classes: [
-        {id: 7, typeId, time: "9:00am - 10:00am", instructor: 'Lara Croft'},
-        {id: 8, typeId, time: '12:00pm - 1:00pm', instructor: 'Sam Flynn'},
-        {id: 9, typeId, time: '5:30pm - 6:30pm', instructor: "Kayla Price"}
-      ]
-    },
-    {
-      id: 4,
-      day: 'thursday',
-      classes: [
-        {id: 10, typeId, time: '9:00am - 10:00am', instructor: 'Izzy Vila'},
-        {id: 11, typeId, time: '11:00am - 12:00pm', instructor: 'Kayla Price'},
-        {id: 12, typeId, time: '5:30pm - 6:30pm', instructor: 'Lara Croft'}
-      ]
-    },
-    {
-      id: 5,
-      day: "friday",
-      classes: [
-        {id: 13, typeId, time: "10:00am - 11:00am", instructor: 'Sam Flynn'},
-        {id: 14, typeId, time: "12:00pm - 1:00pm", instructor: 'Harry Potter'},
-        {id: 15, typeId, time: '3:00pm - 4:00pm', instructor: 'Hermione Granger'}
-      ]
-    },
-    {
-      id: 6,
-      day: "saturday",
-      classes: [
-        {id: 16, typeId, time: '9:00am - 10:00am', instructor: 'Lara Croft'},
-        {id: 17, typeId, time: '10:30am - 11:30am', instructor: 'Lara Croft'},
-        {id: 18, typeId, time: '12:00pm - 1:00pm', instructor: 'Hagrid'}
-      ]
-    }
-];
+  {
+    id: 1,
+    day: "monday",
+    classes: [
+      {id: 1, type: 'foundations', time: '9:00am - 10:00am', instructor: 'Lara Croft'},
+      {id: 2, type: "water", time: '1:00pm - 2:00pm', instructor: 'Jake Sully'},
+      {id: 3, type: "air", time: '5:30pm - 6:30pm', instructor: 'Hannah Cage '}
+    ]
+  },
+  {
+    id: 2,
+    day: "tuesday",
+    classes: [
+      {id: 4, type: "fire", time: '10:30pm - 11:30pm', instructor: "Sam Flynn"},
+      {id: 5, type: 'air', time: "1:00pm - 2:00pm", instructor: 'Hannah Cage'},
+      {id: 6, type: 'water', time: '5:30pm - 6:30pm', instructor: "Kayla Price"}
+    ]
+  },
+  {
+    id: 3,
+    day: "wednesday",
+    classes: [
+      {id: 7, type: "foundations", time: "9:00am - 10:00am", instructor: 'Lara Croft'},
+      {id: 8, type: 'fire', time: '12:00pm - 1:00pm', instructor: 'Sam Flynn'},
+      {id: 9, type: 'water', time: '5:30pm - 6:30pm', instructor: "Kayla Price"}
+    ]
+  },
+  {
+    id: 4,
+    day: 'thursday',
+    classes: [
+      {id: 10, type: 'air', time: '9:00am - 10:00am', instructor: 'Izzy Vila'},
+      {id: 11, type: 'water', time: '11:00am - 12:00pm', instructor: 'Kayla Price'},
+      {id: 12, type: 'foundations', time: '5:30pm - 6:30pm', instructor: 'Lara Croft'}
+    ]
+  },
+  {
+    id: 5,
+    day: "friday",
+    classes: [
+      {id: 13, type: 'fire', time: "10:00am - 11:00am", instructor: 'Sam Flynn'},
+      {id: 14, type: 'air', time: "12:00pm - 1:00pm", instructor: 'Harry Potter'},
+      {id: 15, type: 'water', time: '3:00pm - 4:00pm', instructor: 'Hermione Granger'}
+    ]
+  },
+  {
+    id: 6,
+    day: "saturday",
+    classes: [
+      {id: 16, type: 'foundations', time: '9:00am - 10:00am', instructor: 'Lara Croft'},
+      {id: 17, type: 'foundations', time: '10:30am - 11:30am', instructor: 'Lara Croft'},
+      {id: 18, type: 'water', time: '12:00pm - 1:00pm', instructor: 'Hagrid'}
+    ]
+  }
+]
