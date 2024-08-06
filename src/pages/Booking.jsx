f02b509dc781ea31de1752d2bc4a23837437f913
Carlie <carliemaloneco@gmail.com>
2024-08-06 14:46:08 -0600

    cleaning

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -15,7 +15,7 @@ const Booking = () => {
         <>
         <NavBar />
             <div id='Booking'>
-                <div className='container pt-32'>
+                <div className='container pt-28'>
                     <div className='bg-[#949978] h-[1300px] rounded-t-[4000px]'>
                         <div className='text-[70px] flex justify-center'>
                             <div className='mt-32'>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
         <>
         <NavBar />
             <section id='Contact'>
-                <div className='container pt-32'>
+                <div className='container pt-28'>
                     <div className='bg-[#949978] h-screen rounded-t-[4000px] z-[-1]'>
                         <div className='text-[70px] text-center pt-1'>
                             <div className='mt-32'>
