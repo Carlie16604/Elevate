c84b840f5da0d32aa28eaec465ec18b7e1c7dfec
Carlie <carliemaloneco@gmail.com>
2024-08-13 13:27:52 -0600

    added buttons + title animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import'../index.css';
 import Rock1 from '../assets/Home/Ellipse 1.png'
 import Rock2 from '../assets/Home/Ellipse 2.png'
 import Rock3 from '../assets/Home/Ellipse 3.png'
@@ -32,7 +33,7 @@ const Home = () => {
                                     <h3 className='text-[20px] text-black font-medium uppercase'>
                                         Award Winning Pilates Instructors
                                     </h3>
-                                    <h1 className='text-white text-[120px] font-bold leading-[1.2] '>
+                                    <h1 className='glitch text-white text-[120px] font-bold leading-[1.2]' data-glitch="Pilates">
                                         Pilates
                                     </h1>
                                     <h3 className='text-[30px] text-black font-medium animate-slide-up'>
@@ -41,6 +42,7 @@ const Home = () => {
                                     <p className='animation-slide-up'>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                                     </p>
+                                    <button className='px-4 py-2 bg-[#949978] rounded-[400px]'>Let's Talk About Your Health!</button>
                                 </div>
                                 <div className='flex flex-col'>
                                     <img className='w-[40rem]' src={Stretch} alt='img'/> 
