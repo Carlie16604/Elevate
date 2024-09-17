2918c76270ec97c2f9d0398e524c4422845d0ccb
Carlie <carliemaloneco@gmail.com>
2024-09-17 16:15:39 -0600

    fixed spacing

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -65,7 +65,7 @@ const Reviews = () => {
             </section>
             <section>
                 <div className="my-[5rem] mx-auto">
-                    <div className='flex gap-32 justify-center flex-wrap'>
+                    <div className='flex gap-32 justify-center flex-wrap pt-[3rem]'>
                         <img src={Stretch} alt='stretching person' className="scale-[.8] hover:invert h-32 w-32" style={{transition: '0.3s'}}/>
                         <img src={Core} alt='person stretching their core' className="scale-[.8] hover:invert h-32 w-32" style={{transition: '0.3s'}}/>
                         <img src={Sweat} alt='Sweating person' className="scale-[.8] hover:invert h-32 w-32" style={{transition: '0.3s'}}/>
