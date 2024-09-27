37b9951ef180b53697720ceb8138241636ad4e81
Carlie <carliemaloneco@gmail.com>
2024-09-27 15:00:39 -0600

    added popup function for mobile devices - troubleshooting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './index.css'
 import Main from './pages/Main'
@@ -9,9 +9,34 @@ import Footer from './pages/Components/Footer'
 
 const App = () => {
 
+  // useEffect(() => {
+  //   function checkscreenHeight() {
+  //     const screenHeight = window.innerHeight;
+  //     const messageElement = document.getElementById("desktopMessage");
+
+  //     if (screenHeight > 700) {
+  //       messageElement.classList.remove("hidden");
+  //     } else {
+  //       messageElement.classList.add("hidden");
+  //     }
+  //   }
+
+  //   checkscreenHeight();
+  //   window.addEventListener('resize', checkscreenHeight);
+
+  //   return () => {
+  //     window.removeEventListener('resize', checkscreenHeight);
+  //   };
+  // }, []);
+
   return (
     <Router>
       <>
+        {/* <div id="desktopMessage" className="hidden">
+          <div className="message-content">
+            <p>This is only available on desktop!</p>
+          </div>
+        </div> */}
         <Routes>
           <Route path='/' element={<Main />}/>
           <Route path='/Booking' element={<Booking />}/>
