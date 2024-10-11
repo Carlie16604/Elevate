c1fedfbcbcba15141fe3e73fdb5322a028c4706c
Carlie <carliemaloneco@gmail.com>
2024-10-11 12:49:37 -0600

    saving prog

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                             <img src={Machine} alt='woman on pilates machine' title='woman on pilates machine' className='absolute bottom-0 z-20'/>
                                 <div className='flex'>
                                     <div className='absolute flex flex-col top-0 left-[10%] w-[50%] h-full py-[40px] px-[60px] '>
-                                        <h1 className='text-[60px] text-[#000] my-4 z-10 uppercase font-bold w-[20rem] tracking-wider'>remember why you started</h1>
+                                        <h1 className='text-[60px] text-[#000] my-4 z-10 uppercase font-bold w-[20rem] tracking-wider leading-[4.5rem] pb-5'>remember why you <span className='text-[#FFA366]'>started</span></h1>
                                         <p>At Elevate, we value control and proper alignment to keep the body healthy and pain free. Re-education is an important part of our mission. Our team is here to help you gain knowledge that you can apply to other workouts and everyday life.</p>
                                     </div>
                                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,38 +17,38 @@ const Home = () => {
         <NavBar />
             <section id='Home'>
                 <img src={Studio} alt="studio img" rel='norel' className='z-0 absolute h-[100vh] w-screen min-w-[1200px] object-cover shadow-md'/>
-                <div className='container h-screen'>
-                    <div className='flex flex-wrap'>
-                            <div className='grid grid-cols-2 items-center pt-[250px] pl-12 relative'>
-                                <div>
-                                    <div className='elevate-card top-32 '>
-                                        <h1 className='elevate-card__title'>Elevate</h1>
+                    <div className='container h-screen'>
+                        <div className='flex flex-wrap'>
+                                <div className='grid grid-cols-2 items-center pt-[250px] pl-12 relative'>
+                                    <div>
+                                        <div className='elevate-card top-32 '>
+                                            <h1 className='elevate-card__title'>Elevate</h1>
+                                        </div>
+                                        <h3 className='text-[20px] text-black font-medium uppercase animate-slide-up'>
+                                            Award Winning Pilates Instructors
+                                        </h3>
+                                        <h3 className='pt-[8.5rem] text-[35px] pb-8 text-black font-medium animate-slide-up'>
+                                            Regaining shape, <br/>Relearning you.
+                                        </h3>
+                                        {/* <p className='pt-2 pb-10'>
+                                            Pilates is a renowned Pilates studio dedicated to helping individuals achieve their fitness goals based in Boulder, CO. We host expertly crafted training along with empowering instructors. 
+                                        </p> */}
+                                        <Link to='/Contact' className='text-black border-2 rounded-[400px] border-[#949978] px-10 py-5 inline-block text-[19px] tracking-[1px] cursor-pointer shadow-[inset_0_0_0_0_#949978] transition-[box-shadow] ease-out duration-1000 hover:shadow-[inset_400px_0_0_0_#949978] hover:text-white'>
+                                            Let's Talk About Your Health!
+                                        </Link>
                                     </div>
-                                    <h3 className='text-[20px] text-black font-medium uppercase animate-slide-up'>
-                                        Award Winning Pilates Instructors
-                                    </h3>
-                                    <h3 className='pt-[8.5rem] pb-[4rem] text-[30px] text-black font-medium animate-slide-up'>
-                                        A healthy way to workout
-                                    </h3>
-                                    {/* <p className='pt-2 pb-10'>
-                                        Pilates is a renowned Pilates studio dedicated to helping individuals achieve their fitness goals based in Boulder, CO. We host expertly crafted training along with empowering instructors. 
-                                    </p> */}
-                                    <Link to='/Contact' className='text-black border-2 rounded-[400px] border-[#949978] px-10 py-5 inline-block text-[19px] tracking-[1px] cursor-pointer shadow-[inset_0_0_0_0_#949978] transition-[box-shadow] ease-out duration-1000 hover:shadow-[inset_400px_0_0_0_#949978] hover:text-white'>
-                                        Let's Talk About Your Health!
-                                    </Link>
+                            <div className='blur flex flex-row-reverse -rotate-90 text-white absolute bottom-[25%] right-3 items-center min800:hidden'>
+                                <p className='capitalize text-3xl font-bold tracking-wide'>
+                                    share
+                                </p>
+                                <span className='w-[35px] bg-[#3E5C47] h-[2.5px] mr-6'></span>
+                                <div className='text-white gap-7 flex mr-7'>
+                                    <img className='socials rotate-90' src={Pintrest} alt='Pintrest'/>
+                                    <img className='socials rotate-90' src={Facebook} alt='Facebook'/>
+                                    <img className='socials rotate-90' src={Instagram} alt='Instagram'/>
                                 </div>
-                        <div className='blur flex flex-row-reverse -rotate-90 text-white absolute bottom-[25%] right-3 items-center min800:hidden'>
-                            <p className='capitalize text-3xl font-bold tracking-wide'>
-                                share
-                            </p>
-                            <span className='w-[35px] bg-[#3E5C47] h-[2.5px] mr-6'></span>
-                            <div className='text-white gap-7 flex mr-7'>
-                                <img className='socials rotate-90' src={Pintrest} alt='Pintrest'/>
-                                <img className='socials rotate-90' src={Facebook} alt='Facebook'/>
-                                <img className='socials rotate-90' src={Instagram} alt='Instagram'/>
                             </div>
                         </div>
-                            </div>
                     </div>
                 </div>
             </section>
